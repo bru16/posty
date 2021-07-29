9a9410aea5070aa0bdca0e441b677be69216c3f2
Bruno <bru_16@hotmail.com.ar>
2021-07-29 18:57:51 -0300

    migrations setup & User entity

---
+++ src/entity/User.ts
@@ -0,0 +1,31 @@
+import { Field, ObjectType } from "type-graphql";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  username!: string;
+
+  @Column()
+  password!: string;
+
+  @Field()
+  @CreateDateColumn()
+  created_at: string;
+
+  @Field()
+  @UpdateDateColumn()
+  updated_at: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import { createConnection } from "typeorm";
-import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/post";
 import express from "express";
@@ -9,9 +9,9 @@ import cors from "cors";
 async function bootstrap() {
   try {
     const connection = await createConnection();
+    await connection.runMigrations();
     const app = express();
     app.use(cors());
-
     const apolloServer = new ApolloServer({
       schema: await buildSchema({
         resolvers: [PostResolver],
