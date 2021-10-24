1bc9ee85777ef402a589829d421b5831f746a155
Bruno <bru_16@hotmail.com.ar>
2021-10-23 21:10:07 -0300

    app listen to PORT fixed

--- server/src/index.ts
+++ server/src/index.ts
@@ -60,7 +60,7 @@ async function bootstrap() {
       cors: false,
     }); // --- /graphql endpoint
     const PORT = process.env.PORT || 4000;
-    app.listen(4000, () => console.log(`Listening on PORT: ${PORT}`));
+    app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
   } catch (error) {
     console.error(error);
   }
